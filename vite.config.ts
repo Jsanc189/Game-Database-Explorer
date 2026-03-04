e967159236e262f131966c3630551003fc4e2f8e
Jsanc189 <jackie.sanchez0387@gmail.com>
2026-03-04 09:46:38 -0800

    updates the .json files and vite to deploy on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://jsanc189.github.io/Game-Database-Explorer/"
 });
